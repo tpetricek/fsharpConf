889ad8d92c6d3939adb5a1d98edda974ffcad143
Tomas Petricek <tomas@tomasp.net>
2020-04-26 21:58:44 +0100

    Add fsharpconf 2020 info

--- 2018/design/home.js
+++ 2018/design/home.js
@@ -3,12 +3,12 @@ Number.prototype.pad = function(size) {
   while (s.length < (size || 2)) {s = "0" + s;}
   return s;
 };
-
+    
 $(document).ready(function () {
   function displaytimer() {
-    var millis = (Date.UTC(2020, 5, 5, 9, 0, 0)) - Date.now(); // 11:00 CST = 9:00 UTC
+    var millis = (Date.UTC(2018, 3, 16, 15, 30, 0)) - Date.now(); // 8:30 PDT = 15:30 UTC
     var days = Math.floor(millis / (36e5 * 24)),
-        hours = Math.floor((millis - (days * 24 * 36e5)) / 36e5),
+        hours = Math.floor((millis - (days * 24 *36e5)) / 36e5),
         mins = Math.floor((millis % 36e5) / 6e4),
         secs = Math.floor((millis % 6e4) / 1000);
 
