bbe58d4d4999c4b428b62d4107637aa7ba0b2a10
Tomas Petricek <tomas@tomasp.net>
2023-04-21 10:04:52 +0200

    Add preliminary 2023 info

---
+++ design/home.js
@@ -0,0 +1,21 @@
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+};
+
+/*
+window.onload = function () {
+  function displaytimer() {
+    var millis = (Date.UTC(2020, 5, 5, 9, 0, 0)) - Date.now(); // 11:00 CST = 9:00 UTC
+    var days = Math.floor(millis / (36e5 * 24)),
+        hours = Math.floor((millis - (days * 24 * 36e5)) / 36e5),
+        mins = Math.floor((millis % 36e5) / 6e4),
+        secs = Math.floor((millis % 6e4) / 1000);
+
+    document.getElementById('counter').innerHTML = days + ':' + hours.pad(2) + ':' + mins.pad(2) + ':' + secs.pad(2);
+  };
+  displaytimer();
+  setInterval(displaytimer, 1000);
+};
+*/
